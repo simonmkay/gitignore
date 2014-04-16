548264a379af6ce6b6307de4924162204dae858c
simonmkay <simonmkay@yahoo.com>
2014-04-16 17:30:23 -0400

    Product Store
    I think this is right.

--- PetShop/Pet Shop/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pet_Shop.MvcApplication" Language="C#" %>
---
+++ ProductStore/ProductStore/Models/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Models
+{
+    using System.Collections.Generic;
+
+    public class OrderDTO
+    {
+        public class Detail
+        {
+            public int ProductID { get; set; }
+            public string Product { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+        }
+        public IEnumerable<Detail> Details { get; set; }
+    }
+}
